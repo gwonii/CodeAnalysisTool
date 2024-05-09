e7920a45e821cda3d2c110efd3eabda826a02cf2
hogwon choi <hc.gwonii@gmail.com>
2024-05-09 11:27:16 +0900

    [Fix] openai api 사용하도록 변경

--- Sources/Analyzer.py
+++ Sources/Analyzer.py
@@ -1,14 +1,11 @@
 import os
-import requests
 import tiktoken
+from openai import OpenAI
 
 # GPT-3.5-turbo API 설정
-API_URL = "https://api.openai.com/v1/chat/completions"
-API_KEY = "apy_key"
-HEADERS = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {API_KEY}"
-}
+client = OpenAI(
+    api_key="",
+)
 
 GPT_MODEL = "gpt-3.5-turbo"
 TOKEN_LIMIT_RATIO = 0.8
@@ -38,31 +35,27 @@ def split_into_parts(content):
     return parts
 
 def analyze_code(requirement, code):
-    payload = {
-        "model": GPT_MODEL,
-        "temperature": 0.7,
-        "messages": [
-            {
-                "role": "user",
-                "content": f"{requirement}: \n```\n{code}\n```"
-            }
-        ]
-    }
+    messages = [
+        {
+            "role": "system",
+            "content": "개발 용어를 제외한 나머지는 한글로 대답해줘"
+        },
+        {
+            "role": "assistant",
+            "content": ""
+        },
+        {
+            "role": "user", 
+            "content": f"{requirement}: \n```\n{code}\n```"
+        }
+    ]
 
-    response = requests.post(API_URL, headers=HEADERS, json=payload)
+    completion = client.chat.completions.create(
+        messages=messages,
+        model=GPT_MODEL
+    )
 
-    if response.status_code != 200:
-        print(f"API 요청 실패: HTTP 상태 코드 {response.status_code}")
-        print(f"응답 내용: {response.text}")
-        return None
-
-    try:
-        response_data = response.json()
-    except ValueError:
-        print("응답이 JSON 형식이 아닙니다.")
-        return None
-
-    return response_data['choices'][0]['message']['content'].strip()
+    return completion.choices[0].message.content.strip()
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 
@@ -102,8 +95,7 @@ def process_large_files(directory, file_ext, requirement, output):
 
             write_markdown_report(report_file_path, file_relative_path, parts_analysis)
 
-folder_path = "/Users/gwonii/Documents/iOS/OpenSource/DifferenceKit/Sources"
+folder_path = "/Users/nhn/Documents/OpenSoruces/KarrotListKit/Sources/KarrotListKit"
 file_ext = ".swift"
-requirement = "protocol 별로 property, method 표로 정리해주고 핵심 로직 분석해줘, 그리고 간단한 사용법 및 응용할 수 있는 방법들도 정리해줘"
-print("completed")
-# process_large_files(folder_path, file_ext, requirement, "DifferenceKit.md")
+requirement = "protocol 별로 property, method 표로 정리해주고 핵심 로직 분석해줘, property 의 경우에는 type 도 같이 명시해줘"
+# process_large_files(folder_path, file_ext, requirement, "DifferenceKit.md")""
